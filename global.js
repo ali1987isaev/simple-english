6db11847e82847f11150ea15e5d224a944015783
Ali <ali@tomorrowagency.com>
2023-09-10 05:51:26 +0100

    guess-word.......

--- global.js
+++ global.js
@@ -49,6 +49,12 @@ class Global {
     audioElem.play();
   }
 
+  playGameOver() {
+    const audioElem = new Audio();
+    audioElem.src = './game-over.wav';
+    audioElem.play();
+  }
+
   playClick() {
     const audioElem = new Audio();
     audioElem.src = './click.wav';
@@ -62,12 +68,26 @@ class Global {
     };
   }
 
-  generateVoiceOutput = (message) => {
+  generateVoiceOutput (message) {
     const synth = window.speechSynthesis;
     const utterThis = new SpeechSynthesisUtterance(`${message}`);
     utterThis.lang = "en-US";
     utterThis.rate = 0.9;
     synth.speak(utterThis);
+  }
+
+  generateNumberOfLives(element, counter) {
+    let html = ''
+
+    for (let i = 0; i < counter; i++) {
+      html += `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="heart-icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+      </svg>
+      `
+    };
+
+    element.innerHTML = html;
   }
 }
 
--- guess.js
+++ guess.js
@@ -4,13 +4,16 @@ class Guess {
     this.words = [];
     this.randomArray = [];
     this.currentWord = '';
+    this.lives = 5;
     this.container = document.querySelector('[guess-container]');
     this.theEnd = document.querySelector('[guess-end]');
+    this.gameOver = document.querySelector('[guess-game-over]');
     this.guessWord = this.container.querySelector('[data-guess-word]');
     this.guessOption = this.container.querySelectorAll('[data-guess-option]');
     this.guessWordNumber = this.container.querySelector('[guess-word-number]');
     this.quessNextButton = this.container.querySelector('[data-guess-next-button]');
     this.guessWordVoice = this.container.querySelector('[data-guess-word-voice]');
+    this.numberOfLives = document.querySelector('[data-number-of-lives]');
 
     this.init();
   }
@@ -49,7 +52,19 @@ class Guess {
 
       this.quessNextButton.classList.add('wiggle-animation');
     } else {
-      window.simpleEnglish.global.playError();
+      this.lives > 0 && window.simpleEnglish.global.playError();
+      this.lives--;
+      this.inittNumberOfLives();
+
+      if (this.lives < 3) {
+        this.numberOfLives.querySelectorAll('svg').forEach(el => el.classList.add('scale-animation'));
+      };
+
+      if (this.lives < 1) {
+        this.container.style.display = 'none';
+        this.gameOver.style.display = 'flex';
+        window.simpleEnglish.global.playGameOver();
+      };
     };
   }
 
@@ -105,11 +120,16 @@ class Guess {
     });
   }
 
+  inittNumberOfLives() {
+    window.simpleEnglish.global.generateNumberOfLives(this.numberOfLives, this.lives);
+  }
+
   init() {
     window.simpleEnglish.global.playClick();
     this.initGuessWord();
     this.initNextWord();
     this.initWordVoice();
+    this.inittNumberOfLives();
   }
 }
 
